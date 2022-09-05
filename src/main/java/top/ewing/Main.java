d2999e03a797dd57906a9e792c315d4f350cf17b
kaelvean <yyy_ag@163.com>
2022-09-05 21:39:06 +0800

    test3

--- src/main/java/top/ewing/Main.java
+++ src/main/java/top/ewing/Main.java
@@ -1,7 +1,17 @@
 package top.ewing;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入你的名字：");
+        if (scanner.hasNext()) {
+            String str = scanner.next();
+            System.out.println("你的名字是：" + str);
+        }
         System.out.println("Hello world!\n你好世界！");
+        scanner.close();
     }
 }
