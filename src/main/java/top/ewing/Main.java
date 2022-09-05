9b09325925d824d21514578c23e8af971f4994f5
kaelvean <yyy_ag@163.com>
2022-09-05 21:36:25 +0800

    test

--- src/main/java/top/ewing/Main.java
+++ src/main/java/top/ewing/Main.java
@@ -2,6 +2,6 @@ package top.ewing;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!\n你好世界！");
+        System.out.println("Hello world!你好世界！");
     }
 }
